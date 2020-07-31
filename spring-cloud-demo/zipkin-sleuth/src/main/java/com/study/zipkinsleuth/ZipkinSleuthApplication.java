29307c764451388559f1f8d577ddb9ade825d50c
RookieToElitist <411884606@qq.com>
2020-07-31 16:47:44 +0800

    学习分布式链路跟踪sleuth之整合Zipkin日志聚合服务

--- spring-cloud-demo/zipkin-sleuth/src/main/java/com/study/zipkinsleuth/ZipkinSleuthApplication.java
+++ spring-cloud-demo/zipkin-sleuth/src/main/java/com/study/zipkinsleuth/ZipkinSleuthApplication.java
@@ -2,7 +2,10 @@ package com.study.zipkinsleuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
 
+//开启
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinSleuthApplication {
 
