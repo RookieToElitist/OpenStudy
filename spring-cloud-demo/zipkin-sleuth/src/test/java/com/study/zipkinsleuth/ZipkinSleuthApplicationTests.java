d31ca8466a60fe36347c69cde2924ccbbeac2290
RookieToElitist <411884606@qq.com>
2020-07-31 16:43:02 +0800

    学习分布式链路跟踪sleuth之整合Zipkin日志聚合服务

---
+++ spring-cloud-demo/zipkin-sleuth/src/test/java/com/study/zipkinsleuth/ZipkinSleuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.zipkinsleuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinSleuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
