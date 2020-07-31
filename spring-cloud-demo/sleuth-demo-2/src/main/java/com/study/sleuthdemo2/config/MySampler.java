81b07222459d0af680556fe298a0a6f05483addc
RookieToElitist <411884606@qq.com>
2020-07-31 16:41:14 +0800

    学习分布式链路跟踪sleuth之抽样策略

--- spring-cloud-demo/sleuth-demo-2/src/main/java/com/study/sleuthdemo2/config/MySampler.java
+++ spring-cloud-demo/sleuth-demo-2/src/main/java/com/study/sleuthdemo2/config/MySampler.java
@@ -1,8 +1,11 @@
-package com.study.sleuthdemo2;
+package com.study.sleuthdemo2.config;
 
 import org.springframework.cloud.sleuth.Sampler;
 import org.springframework.cloud.sleuth.Span;
 
+/**
+ * 自定义一个抽样策略：只含有tag
+ */
 public class MySampler implements Sampler {
 
     private String tag;
--- spring-cloud-demo/sleuth-demo-2/src/main/java/com/study/sleuthdemo2/config/SleuthSamperConfig.java
+++ spring-cloud-demo/sleuth-demo-2/src/main/java/com/study/sleuthdemo2/config/SleuthSamperConfig.java
@@ -1,4 +1,17 @@
 package com.study.sleuthdemo2.config;
 
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Samper的实现类，使得返回值永远是true，这样使得Sleuth不使用默认的抽样比例
+ */
+@Configuration
 public class SleuthSamperConfig {
+
+    @Bean
+    public AlwaysSampler alwaysSampler(){
+        return new AlwaysSampler();
+    }
 }
