756a5ee6683332a4c5541378c3cce8d604e20d31
RookieToElitist <411884606@qq.com>
2020-07-31 14:36:10 +0800

    学习分布式链路跟踪sleuth之探究请求头携带Span类属性

--- spring-cloud-demo/sleuth-demo-1/src/main/java/com/study/sleuthdemo/controller/SleuthController.java
+++ spring-cloud-demo/sleuth-demo-1/src/main/java/com/study/sleuthdemo/controller/SleuthController.java
@@ -5,16 +5,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
 @RestController
 public class SleuthController {
 
     @Autowired
     RestTemplate rt;
 
-    @GetMapping
+    @GetMapping(value = "/sleuth1")
     String sleuth1(){
         System.out.println("请求sleuth1");
         String body = rt.getForEntity("http://SLEUTH-DEMO-2/sleuth2", String.class).getBody();
+        return "先请求了sleuth1，再"+body;
+    }
+
+    /**
+     * 测试获取Sleuth在请求头添加的信息
+     * @return
+     */
+    @GetMapping(value = "/sleuthHeaderInfo")
+    String sleuthHeaderInfo(){
+        String body = rt.getForEntity("http://SLEUTH-DEMO-2/sleuthHeaderInfo", String.class).getBody();
         return "先请求了sleuth1，再"+body;
     }
 }
--- spring-cloud-demo/sleuth-demo-2/src/main/java/com/study/sleuthdemo2/controller/SleuthController.java
+++ spring-cloud-demo/sleuth-demo-2/src/main/java/com/study/sleuthdemo2/controller/SleuthController.java
@@ -5,15 +5,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
 @RestController
 public class SleuthController {
 
     @Autowired
     RestTemplate rt;
 
-    @GetMapping
+    @GetMapping(value = "/sleuth2")
     String sleuth2(){
         System.out.println("请求sleuth2");
+        return "请求了sleuth2";
+    }
+
+    /**
+     * 主要为了验证请求头上含有Span类的属性信息
+     * @param request
+     * @return
+     */
+    @GetMapping(value = "/sleuthHeaderInfo")
+    String sleuthHeaderInfo(HttpServletRequest request){
+        System.out.println("获取所有请求头信息===》》》");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + "=" + value);
+        }
+
         return "请求了sleuth2";
     }
 }
