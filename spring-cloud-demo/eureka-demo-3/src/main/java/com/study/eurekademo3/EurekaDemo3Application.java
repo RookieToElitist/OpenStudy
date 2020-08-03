b21f98b86a9059490f75f7c2f0b893b9a9aed6c6
RookieToElitist <411884606@qq.com>
2020-08-03 16:52:26 +0800

    学习搭建集群版eureka服务注册中心

---
+++ spring-cloud-demo/eureka-demo-3/src/main/java/com/study/eurekademo3/EurekaDemo3Application.java
@@ -0,0 +1,13 @@
+package com.study.eurekademo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDemo3Application.class, args);
+	}
+
+}
