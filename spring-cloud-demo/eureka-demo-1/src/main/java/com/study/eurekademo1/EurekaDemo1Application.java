f18bc9f7f27b1bd299154e4a656ef2eeaa8d692f
RookieToElitist <411884606@qq.com>
2020-08-03 16:51:53 +0800

    学习搭建单机版eureka服务注册中心

---
+++ spring-cloud-demo/eureka-demo-1/src/main/java/com/study/eurekademo1/EurekaDemo1Application.java
@@ -0,0 +1,13 @@
+package com.study.eurekademo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDemo1Application.class, args);
+	}
+
+}
