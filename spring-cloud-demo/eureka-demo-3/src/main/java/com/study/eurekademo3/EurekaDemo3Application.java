739a32cf6f64a77c8619d74597329569e86195fb
RookieToElitist <411884606@qq.com>
2020-08-03 16:56:22 +0800

    学习搭建集群版eureka服务注册中心

--- spring-cloud-demo/eureka-demo-3/src/main/java/com/study/eurekademo3/EurekaDemo3Application.java
+++ spring-cloud-demo/eureka-demo-3/src/main/java/com/study/eurekademo3/EurekaDemo3Application.java
@@ -2,7 +2,10 @@ package com.study.eurekademo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//开启Eureka服务
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaDemo3Application {
 
