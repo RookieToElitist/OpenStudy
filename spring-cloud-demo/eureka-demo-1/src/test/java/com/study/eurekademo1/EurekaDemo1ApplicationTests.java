a27e7afc51f9be4f83d4fad72e0e5a446995c33e
RookieToElitist <411884606@qq.com>
2020-08-05 15:00:46 +0800

    20200805-杂项

--- spring-cloud-demo/eureka-demo-1/src/test/java/com/study/eurekademo1/EurekaDemo1ApplicationTests.java
+++ spring-cloud-demo/eureka-demo-1/src/test/java/com/study/eurekademo1/EurekaDemo1ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.study.eurekademo1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EurekaDemo1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EurekaDemo1ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
