23d216cf49bb8bcee39562fc10e244bc1e2669b2
RookieToElitist <411884606@qq.com>
2020-08-26 11:42:55 +0800

    删除

--- 5g-msg-platform/src/main/java/com/bankcomm/MsgPlatform/service/SendTemplateService.java
+++
@@ -1,9 +0,0 @@
-package com.bankcomm.MsgPlatform.service;
-
-import com.bankcomm.MsgPlatform.entity.MessageTemplate;
-import okhttp3.OkHttpClient;
-
-public interface SendTemplateService {
-
-    public void sendTemplate(MessageTemplate messageTemplate, OkHttpClient client);
-}
