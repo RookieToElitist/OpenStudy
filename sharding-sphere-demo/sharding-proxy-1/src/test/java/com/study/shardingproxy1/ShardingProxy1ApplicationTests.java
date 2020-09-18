1d0e9ab4e0c2db4ad33185ec79286dd9e87bd124
RookieToElitist <411884606@qq.com>
2020-09-18 16:30:57 +0800

    shardidng-proxy二进制安装包及简单配置

---
+++ sharding-sphere-demo/sharding-proxy-1/src/test/java/com/study/shardingproxy1/ShardingProxy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.shardingproxy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingProxy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
